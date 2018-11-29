36402155d271b86d50fcc7f52645447d8fe09a8a
lucianamarques <lucianadacostamarques@gmail.com>
2018-11-29 15:07:50 -0200

    Initial Commit

---
+++ Orientations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Orientations
+//
+//  Created by Luciana on 29/11/18.
+//  Copyright © 2018 Luciana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
